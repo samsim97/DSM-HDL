0339bcd08967bcf05810e56f81c6479a94479ac5
samsim97 <samuel.simard97@hotmail.com>
2026-01-23 13:08:40 -0500

    Changed the parameters of the dsm

--- src/rtl/dsm_dac.v
+++ src/rtl/dsm_dac.v
@@ -10,14 +10,16 @@ module dsm_dac #(
 );
 
   localparam integer FEEDBACK_MAG = 1 << (DATA_WIDTH - 1);
-  localparam integer DELTA_WIDTH  = DATA_WIDTH + 1;
+  localparam integer ADDITIONAL_DELTA_WIDTH = 1;
+  localparam integer DELTA_WIDTH  = DATA_WIDTH + ADDITIONAL_DELTA_WIDTH;
 
+  // wire signed [DELTA_WIDTH:0] w_delta_out;
   wire signed [DELTA_WIDTH-1:0] w_delta_out;
   wire w_integrator_quantizer_out;
 
   delta_feedback #(
     .DATA_WIDTH(DATA_WIDTH),
-    .ADDITIONAL_DELTA_WIDTH(DELTA_WIDTH - DATA_WIDTH),
+    .ADDITIONAL_DELTA_WIDTH(ADDITIONAL_DELTA_WIDTH),
     .FEEDBACK_MAG(FEEDBACK_MAG)
   ) u_delta_feedback (
     .i_data(i_data),
@@ -27,6 +29,7 @@ module dsm_dac #(
 
   integrator_quantizer #(
     .DATA_WIDTH(DELTA_WIDTH), 
+    .ADDITIONAL_DELTA_WIDTH(ADDITIONAL_DELTA_WIDTH),
     .ACC_WIDTH(ACC_WIDTH)
   ) u_integrator_quantizer (
     .i_clk(i_clk),
