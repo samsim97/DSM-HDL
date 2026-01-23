1b4f0fb9d257572c7e295c3347a8a88d335f887e
samsim97 <samuel.simard97@hotmail.com>
2026-01-23 14:03:38 -0500

    Created a compressed version of the dsm_dac module to fit in one file

--- src/rtl/dsm_dac.v
+++ src/rtl/dsm_dac.v
@@ -1,6 +1,6 @@
 module dsm_dac #(
-  parameter integer DATA_WIDTH   = 16,
-  parameter integer ACC_WIDTH    = DATA_WIDTH + 3
+  parameter integer DATA_WIDTH = 16,
+  parameter integer ACC_WIDTH  = DATA_WIDTH + 3
 ) (
   input  wire i_clk,
   input  wire i_rst_n,
@@ -9,34 +9,29 @@ module dsm_dac #(
   output wire o_dac_bitstream
 );
 
-  localparam integer FEEDBACK_MAG = 1 << (DATA_WIDTH - 1);
-  localparam integer ADDITIONAL_DELTA_WIDTH = 1;
-  localparam integer DELTA_WIDTH  = DATA_WIDTH + ADDITIONAL_DELTA_WIDTH;
+  localparam integer ADDITIONAL_DELTA_WIDTH        = 1;
+  localparam integer DELTA_WIDTH                   = DATA_WIDTH + ADDITIONAL_DELTA_WIDTH;
+  localparam signed [DELTA_WIDTH-1:0] FEEDBACK_MAG = 1 << (DATA_WIDTH - 1);
 
   wire signed [DELTA_WIDTH-1:0] w_delta_out;
-  wire w_integrator_quantizer_out;
+  // Scale i_data to DELTA_WIDTH
+  wire signed [DELTA_WIDTH-1:0] w_delta_data_in_ext = {{(DELTA_WIDTH-DATA_WIDTH){i_data[DATA_WIDTH-1]}}, i_data};
 
-  delta_feedback #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .ADDITIONAL_DELTA_WIDTH(ADDITIONAL_DELTA_WIDTH),
-    .FEEDBACK_MAG(FEEDBACK_MAG)
-  ) u_delta_feedback (
-    .i_data(i_data),
-    .i_quantized_bit(w_integrator_quantizer_out),
-    .o_delta(w_delta_out)
-  );
+  reg signed [ACC_WIDTH-1:0] r_int_data;
+  // Scale w_delta_out to ACC_WIDTH
+  wire signed [ACC_WIDTH-1:0] w_int_data_ext = {{(ACC_WIDTH-DELTA_WIDTH){w_delta_out[DELTA_WIDTH-1]}}, w_delta_out};
+  wire w_quantize_bit;
 
-  integrator_quantizer #(
-    .DATA_WIDTH(DELTA_WIDTH), 
-    .ACC_WIDTH(ACC_WIDTH)
-  ) u_integrator_quantizer (
-    .i_clk(i_clk),
-    .i_rst_n(i_rst_n),
-    .i_en(i_en),
-    .i_data(w_delta_out),
-    .o_data(w_integrator_quantizer_out)
-  );
+  always @(posedge i_clk or negedge i_rst_n) begin
+    if (!i_rst_n) begin
+      r_int_data <= '0;
+    end else if (i_en) begin
+      r_int_data <= r_int_data + w_int_data_ext;
+    end
+  end
 
-  assign o_dac_bitstream = w_integrator_quantizer_out;
+  assign w_delta_out     = w_delta_data_in_ext - (w_quantize_bit ? FEEDBACK_MAG : -FEEDBACK_MAG);
+  assign w_quantize_bit  = (r_int_data >= 0) ? 1'b1 : 1'b0;
+  assign o_dac_bitstream = w_quantize_bit;
 
 endmodule
---
+++ src/rtl/dsm_dac_old.v
@@ -0,0 +1,42 @@
+module dsm_dac_old #(
+  parameter integer DATA_WIDTH   = 16,
+  parameter integer ACC_WIDTH    = DATA_WIDTH + 3
+) (
+  input  wire i_clk,
+  input  wire i_rst_n,
+  input  wire i_en,
+  input  wire signed [DATA_WIDTH-1:0] i_data,
+  output wire o_dac_bitstream
+);
+
+  localparam integer FEEDBACK_MAG = 1 << (DATA_WIDTH - 1);
+  localparam integer ADDITIONAL_DELTA_WIDTH = 1;
+  localparam integer DELTA_WIDTH  = DATA_WIDTH + ADDITIONAL_DELTA_WIDTH;
+
+  wire signed [DELTA_WIDTH-1:0] w_delta_out;
+  wire w_integrator_quantizer_out;
+
+  delta_feedback #(
+    .DATA_WIDTH(DATA_WIDTH),
+    .ADDITIONAL_DELTA_WIDTH(ADDITIONAL_DELTA_WIDTH),
+    .FEEDBACK_MAG(FEEDBACK_MAG)
+  ) u_delta_feedback (
+    .i_data(i_data),
+    .i_quantized_bit(w_integrator_quantizer_out),
+    .o_delta(w_delta_out)
+  );
+
+  integrator_quantizer #(
+    .DATA_WIDTH(DELTA_WIDTH), 
+    .ACC_WIDTH(ACC_WIDTH)
+  ) u_integrator_quantizer (
+    .i_clk(i_clk),
+    .i_rst_n(i_rst_n),
+    .i_en(i_en),
+    .i_data(w_delta_out),
+    .o_data(w_integrator_quantizer_out)
+  );
+
+  assign o_dac_bitstream = w_integrator_quantizer_out;
+
+endmodule
